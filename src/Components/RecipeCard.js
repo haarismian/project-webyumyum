6873ff0dd1eb89188cc2a892b6afc7780e32510f
Haaris Mian <mail@haarismian.com>
2020-08-22 01:11:12 -0400

    initial commit

---
+++ src/Components/RecipeCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Card } from 'antd';
+
+export default class RecipeCard extends React.Component {
+  render() {
+    const { Meta } = Card;
+
+    return (
+      <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={
+          <img
+            alt="example"
+            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          />
+        }
+      >
+        <Meta title="Europe Street beat" description="www.instagram.com" />
+      </Card>
+    );
+  }
+}
---
+++ src/Pages/RecipeGallery.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import RecipeCard from '../Components/RecipeCard';
+
+export default class Welcome extends React.Component {
+  render() {
+    return <RecipeCard />;
+  }
+}
